735cdacf91c85b10772d86f564511c66df3be66d
Jankovik <84988190+Jankovik@users.noreply.github.com>
2021-10-04 14:57:20 +0200

    Add files via upload

---
+++ Valorant-Cheat-main/README.md
@@ -0,0 +1,25 @@
+<div align=center>
+  
+  <img src="./images/valorantLogo.png" width=100>
+  
+  # VALORANT Cheat
+  ```
+  💎 VALORANT triggerbot built in Python (use at your own risk!)
+  ```
+  
+</div>
+
+https://user-images.githubusercontent.com/44041512/118185317-402b4580-b3f1-11eb-98f6-46be9f5559ec.mp4
+
+## Instructions
+1. [Download](https://github.com/b0kch01/valorant-cheat/releases/latest) the executable `NotASketchFileName.exe`
+2. Run the executable (make sure to allow admin rights; click yes or login if Windows asks)
+3. Hold <kbd>alt</kbd> to activate trigger bot. You can change this keybind in the app.
+4. Play in **Windowed Fullscreen**.
+
+## Learn the process!
+https://youtu.be/zoSNR6Yx3kg
+
+## 💀 USE AT YOUR OWN RISK 💀
+- I am not responsible for anything that happens
+- Anyways, use responsibly :)
---
+++ Valorant-Cheat-main/main.py
@@ -0,0 +1,139 @@
+# Valorant (any game really) trigger bot
+# Made by Jankovik
+
+import colorama
+from termcolor import cprint, colored
+from pyfiglet import Figlet
+from pynput.mouse import Controller, Button
+from pynput import mouse
+import numpy as np
+import d3dshot
+import time
+import keyboard
+import win32gui
+import win32api
+import os
+import ctypes
+
+# Disable quick-edit mode (pauses bot)
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
+
+# Fix legacy console color
+colorama.init()
+
+# CONSTANTS
+KEYBIND = "alt"  # Default keybind
+BOX_LENGTH = 4  # Screen capture size
+SCREEN_X = win32api.GetSystemMetrics(0)  # Auto-fetched (doesn't always work)
+SCREEN_Y = win32api.GetSystemMetrics(1)
+
+# Calculating box coorinates
+X1 = int(SCREEN_X/2 - BOX_LENGTH/2)
+Y1 = int(SCREEN_Y/2 - BOX_LENGTH/2)
+X2 = int(X1 + BOX_LENGTH)
+Y2 = int(Y1 + BOX_LENGTH)
+
+REGION = (X1, Y1, X2, Y2)
+
+cprint("Setting up...")
+cprint(" - [¤] Windows", "green")
+cprint(f" - [¤] {SCREEN_X}x{SCREEN_Y}", "green")
+
+if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+    cprint(" - [x] Please run as administrator", "red")
+    exit(0)
+
+cprint(f" - [¤] {SCREEN_X}x{SCREEN_Y}", "green")
+
+time.sleep(0.5)
+
+# Disable click delay (100ms)
+win32gui.GetDoubleClickTime = lambda: 0
+
+# Instantiate mouse controller
+mouse = Controller()
+
+# Instantiate screen capture (numpy is the fastest)
+d = d3dshot.create(capture_output="numpy")
+
+
+# Grabs center screen as an average pixel value
+def rgb_pixel():
+    return np.average(d.screenshot(region=REGION))
+
+
+# Given start time, returns time elapsed in ms
+def time_elapsed(start_time):
+    return str(int((time.time() - start_time)*1000)) + "ms"
+
+
+# User Interface
+f = Figlet(font="ogre")
+
+CACHED_TITLESCREEN = f"""
+{ f.renderText("Valorant Cheat")[:-3] }
+{ colored(" Created with ♥ by b0kch01! ", "grey", "on_white") }
+{ colored(" USE AT YOUR OWN RISK       ", "grey", "on_yellow") }
+
+Enjoy! :)
+"""
+
+
+# Clears console
+def clear():
+    os.system("cls")
+
+
+# Prints titlescreen
+def titlescreen():
+    clear()
+    print(CACHED_TITLESCREEN)
+
+
+# MAIN SCRIPT
+try:
+    titlescreen()
+    if input("Set custom keybind? (yes/no): ")[:1] in "yY":
+        titlescreen()
+        print(f"Current keybind: [{colored(KEYBIND, 'green')}]")
+        print("\nPress [ESC] to continue")
+
+        new_key = KEYBIND
+
+        while True:
+            new_key = keyboard.read_key()
+
+            if new_key == "esc":
+                break
+            elif new_key != KEYBIND:
+                KEYBIND = new_key
+                titlescreen()
+                print(f"Current keybind: [{colored(KEYBIND, 'green')}]")
+                print("\nPress [ESC] to continue")
+
+    titlescreen()
+    print(f"Current keybind: [{colored(KEYBIND, 'green')}]")
+    cprint("\nReady to frag.", "green")
+
+    current_pixel = rgb_pixel()
+
+    while True:
+        if keyboard.is_pressed(KEYBIND):
+            timeS = time.time()
+            new_pixel = rgb_pixel()
+
+            if abs(current_pixel - new_pixel) > 5:
+                mouse.click(Button.left)
+                print("[¤] Clicked within " + time_elapsed(timeS))
+
+            current_pixel = new_pixel
+        else:
+            current_pixel = rgb_pixel()
+            time.sleep(0.05)
+
+except KeyboardInterrupt:
+    pass
+
+cprint("\n~ Program exited ;-;", "grey", "on_red")
+time.sleep(1)
